f6a75dae763468d4048c484645da739389e39143
brunosaunders <senabruno97@hotmail.com>
2023-01-17 19:23:15 -0300

    web setup

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+      <Habit completed={0}/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,10 @@
+interface HabitProps {
+    completed: number
+}
+
+export function Habit(props: HabitProps) {
+    return (
+        <div className="bg-zinc-900 h-24 w-20 text-white rounded m-2 flex items-center justify-center"
+        >Hábitos: {props.completed}</div>
+    )
+}
