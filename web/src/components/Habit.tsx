89b85453e34e683405471f1a74c93e352140df73
brunosaunders <senabruno97@hotmail.com>
2023-01-17 21:11:25 -0300

    mobile setup finished

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -7,4 +7,4 @@ export function Habit(props: HabitProps) {
         <div className="bg-zinc-900 h-24 w-20 text-white rounded m-2 flex items-center justify-center"
         >Hábitos: {props.completed}</div>
     )
-}
+} 
